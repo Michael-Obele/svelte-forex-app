870ee3767f4a1861b33a45866f03f3fd69dacff1
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-10-26 19:20:31 +0100

    Testing SDK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script>
+	import * as Ably from 'ably';
+	let apiKey = 'pmPj8A.10JMLQ:kVdYpkwf-_c_2SEftDg9IJlFw1SblYaw5DTAd8G3zCw';
+	const ably = new Ably.Realtime.Promise({ key: apiKey });
+
+	let time = 'Sjwejw';
+
+	let channel = ably.channels.get('time');
+
+	setInterval(() => {
+		channel.publish('update', { Time: Date.now().toString() });
+	}, 3000);
+
+	let sub = channASXel.subscribe((msg) => {
+		time = JSON.stringify(msg.data);
+	});
+</script>
+
+<svelte:head>
+	<script src="https://cdn.ably.com/lib/ably.min-1.js"></script>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h3>{time}</h3>
