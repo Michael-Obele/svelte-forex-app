e36d0f863c1c0c38057cd7521830a99ac0800f6a
Michael Obubelebra Amachree <amachree9630@gmail.com>
2023-11-02 21:10:34 +0100

    ts fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,25 @@
 <script>
-	import * as Ably from 'ably';
+	//@ts-nocheck
 	let apiKey = 'pmPj8A.10JMLQ:kVdYpkwf-_c_2SEftDg9IJlFw1SblYaw5DTAd8G3zCw';
 	const ably = new Ably.Realtime.Promise({ key: apiKey });
+	console.log('apiKey = ', apiKey);
 
 	let time = 'Sjwejw';
 
 	let channel = ably.channels.get('time');
 
 	setInterval(() => {
-		channel.publish('update', { Time: Date.now().toString() });
+		channel.publish('update', { Time: Date.now() });
 	}, 3000);
 
 	let sub = channel.subscribe((msg) => {
 		time = JSON.stringify(msg.data);
 	});
 </script>
+
+<svelte:head>
+	<script src="https://cdn.ably.com/lib/ably.min-1.js"></script>
+</svelte:head>
 
 <h1>Welcome to SvelteKit</h1>
 
